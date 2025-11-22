8c73f315f245741aa0d326858f84eaa4c5591f99
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-22 19:45:21 +0000

    this is standart commit for task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """This is a Square class."""
-   
-    def __init__(self, size = 0):
-     if not isinstance(size, int):
-         raise TypeError("size must be an integer")
-     if size < 0 :
-        raise ValueError("size must be >= 0")
-    
-     self.__size = size
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
